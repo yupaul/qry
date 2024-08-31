2fba95bbd8119cb2a034c87098fdd6492596d618
yp <yp@example.com>
2024-08-31 03:32:18 +0100

    0.002

--- qry.js
+++ qry.js
@@ -1,73 +1,101 @@
 const mysql = require('mysql2')
 const { copyObj, parseJson, parseFlatted, stringifyFlatted, sNull, unsNull, prepPackrPackData } = require('./helper')
-const RedisUtilFunctions = require('./redis-util-functions/redis-util-functions');
+const { Packr } = require('msgpackr')
+const refuncs = require('./redis-util-functions/redis-util-functions');
 
-class DbConnection {
+let db = null
 
-	constructor() {
-		this.redis = RedisUtilFunctions
-		this.pool = null
-		this.pool_archive = null
-		this.db = null
-		this.redisClient = null		
-		this.scServer = null
-		this.videoFootages = []
-		this.intercom = null
-		this.activeAutomations = []
-		this.redisHprefix = process.env.REDIS_HPREFIX ? process.env.REDIS_HPREFIX : false
-		this.STOP_FP = false
-	}
+let _default_options = {
+	rkey: null,
+	rpath: null,
+	r_packr: null,
+	rmulti: null,
+	rjson: null,
+	rfield: null,
+	rjson_flatted: false,
+	rnoread: false,
+	rcb: null,
+	rexpire: 0,
+	rawait: false,
+	rdel: null,
+	rkeepttl: false,
+	more_data: null,
+	silent_error: false,
+}
 
-	setPoolEvents(opts) {
-		let pool_key = opts && opts.pool ? opts.pool : 'pool'
-		this[pool_key].on('connection', (connection) => {
-			logger.CLOG('DB Connection established.'+(pool_key !== 'pool' ? ' ('+pool_key+')' : ''), 'force')
+const setOptions = function(options, defaults) {
+	const out_options = Object.assign({}, defaults)
+	if (!options || typeof options !== 'object') return out_options
 
-			connection.on('error', async (err) => {
-				logger.CLOGJ('DB Connection error'+(pool_key !== 'pool' ? ' ('+pool_key+')' : ''), err, 'force')
-				logger.ERROR.error(logger.lineNumber(new Error()) + ' | ' + err)
-				//this.open()
-				await this.createPool(false, opts)
-			})
-			connection.on('close', function (err) {
-				console.error(new Date(), 'DB Connection close'+(pool_key !== 'pool' ? ' ('+pool_key+')' : ''), err)
-			});
-		})		
-	}
-
-	async createPool(dont_end, opts) {
-		let pool_key = 'pool'
-		if(opts) {
-			if(opts.pool) pool_key = opts.pool
-		} else {
-			opts = process.env
-		}
-		if (this[pool_key] && !dont_end) {
-			await new Promise((resolve, reject) => {
-				this[pool_key].end((err) => {
-					setTimeout(() => {
-						this.createPool(true, opts)
-						return resolve(1)
-					}, 50);
-				})
-			})
-		} else {
-			this[pool_key] = mysql.createPool({
-				host: opts.DB_HOSTNAME,
-				user: opts.DB_USERNAME,
-				password: opts.DB_PASSWORD,
-				database: opts.DB_NAME,
-
-				connectTimeout: 20000,
-				queueLimit: 0,
-				//connectionLimit: 10000,
-				waitForConnections: true,				
-			})
+	for (let k in out_options) {
+		if (!out_options.hasOwnProperty(k) || options[k] === out_options[k]) continue
+		if (typeof out_options[k] === 'boolean') {
+			out_options[k] = Boolean(options[k])
+			continue
+		} else
+		if (['rpath', 'rmulti', 'rjson', 'rcb', 'more_data', ].indexOf(k) !== -1) {
+			out_options[k] = options[k]
+			continue
+		} else
+		if (['rkey', 'rfield', ].indexOf(k) !== -1) {
+			if ((typeof options[k] === 'string' && options[k].length) || typeof options[k] === 'function') out_options[k] = options[k]			
+		} else 
+		if (k === 'rexpire') {
+			const _rexpire = Number(options.rexpire)
+			out_options.rexpire = Number.isInteger( _rexpire) && _rexpire > 0 ? _rexpire : 0			
+		} else 
+		if (k === 'r_packr') {
+			if(options.r_packr && options.r_packs instanceof Packr) out_options.r_packr = options.r_packr			
+		} else 
+		if (k === 'rdel') {
+			if (options.rdel && (typeof options.rdel === 'string' || (Array.isArray(options.rdel) && options.rdel.length))) out_options.rdel = options.rdel
 		}
 	}
+	return out_options
+}
 
 
-	async qry(query, query_args, cb, opts) {
+/**
+ * Execute a query on the database and/or Redis.
+ * @param {string} query - the SQL query to execute
+ * @param {Array} query_args - the arguments for the query
+ * @param {function} cb - the callback function to call with the result
+ * @param {Object} opts - options object
+ * @param {Object} [opts.db] - the database connection to use
+ * @param {boolean} [opts.redis] - whether to use Redis
+ * @param {string} [opts.rkey] - the Redis key to use
+ * @param {string} [opts.rpath] - the Redis path to use
+ * @param {string} [opts.rfield] - the Redis field to use
+ * @param {boolean} [opts.rmulti] - whether to use Redis multi commands
+ * @param {boolean} [opts.rjson] - whether to store the value in Redis as JSON
+ * @param {boolean} [opts.rjson_flatted] - whether to store the value in Redis as flatted JSON
+ * @param {Packr} [opts.r_packr] - the packr instance to use for packing/unpacking values
+ * @param {boolean} [opts.rnoread] - whether to not read the value from Redis
+ * @param {boolean} [opts.rawait] - whether to wait for the Redis write to complete before resolving the Promise
+ * @param {boolean} [opts.rkeepttl] - whether to keep the Redis TTL
+ * @param {number} [opts.rexpire] - the Redis TTL to set
+ * @param {string|string[]} [opts.rdel] - the Redis key(s) to delete
+ * @param {function} [opts.rcb] - the callback function to call with the value from Redis
+ * @param {boolean} [opts.silent_error] - whether to not throw an error if the database query fails
+ * @returns {Promise} a Promise resolved with the result of the query
+ */
+const qry = async (query, query_args, cb, opts) => {
+		let _db = null
+		if (opts.db) {
+			_db = opts.db			
+			if (!db) db = _db
+		} else {
+			if (!db) return new Promise((resolve, reject) => reject('DB Connection Required'))
+			_db = db
+		}
+
+		if (opts.redis) {
+			refuncs.setClient(opts.redis)
+		} else 	if (!refuncs.redisClient) {
+			return new Promise((resolve, reject) => reject('Redis Connection Required'))	
+		}
+			
+
 		let redis_result = null
 		let _args
 		let _cb
@@ -80,6 +108,8 @@ class DbConnection {
 				opts = {}
 			}
 		}
+		opts = setOptions(opts, _default_options)
+
 		if (opts.rkey) {
 			if (opts.r_packr) {
 				if (opts.rmulti) {
@@ -133,11 +163,10 @@ class DbConnection {
 					} else if (redis_methods[0] === 'lrangeBuffer') {
 						_args.splice(2, 0, 0, -1)					
 					}
-					this.r(..._args)
+					refuncs.r(..._args)
 				})
 				if (redis_result && redis_methods[0] === 'hgetall') redis_result = !Object.keys(redis_result).length ? null : unsNull(redis_result)
 			} catch (_exc) {
-				logger.CLOGJ(logger.lineNumber(new Error(), 2), _exc, 'force')
 				redis_result = null
 			}
 		}
@@ -150,13 +179,8 @@ class DbConnection {
 
 		let f = (resolve, reject) => {
 			if (redis_result !== null || !query) return resolve(redis_result)
-			//if(Array.isArray(query_args)) query_args = ar2null(query_args)
-			let _db = opts.db ? opts.db : this.pool
 			_db.query(query, query_args || [], async (db_err, db_result) => {
-				if (db_err) {
-					logger.CLOGJ(logger.lineNumber(new Error(), 2) + ' | ' + db_err + ' SQL: ' + db_err.sql, 'force')
-					return (opts.silent_error ? resolve(null) : reject(db_err))
-				}
+				if (db_err) return (opts.silent_error ? resolve(null) : reject(db_err))
 
 				let cb_result = _cb(db_result, reject, opts.more_data)
 				if (cb_result instanceof Promise) cb_result = await cb_result
@@ -177,7 +201,7 @@ class DbConnection {
 							_commands.push(['hdel', ..._rdel])
 						}
 					})
-					if (_commands.length) _commands.length > 1 ? this.rpipemulti(_commands) : this.r(..._commands[0])					
+					if (_commands.length) _commands.length > 1 ? refuncs.rpipemulti(_commands) : refuncs.r(..._commands[0])					
 				}
 
 				if (redis_methods && cb_result !== null) {
@@ -196,8 +220,7 @@ class DbConnection {
 					} else {
 						_args.push(opts.rjson ? json_stringify_func(cb_result) : (redis_methods[0] === 'hgetall' ? sNull(cb_result) : cb_result))
 					}
-
-					if (!opts.rexpire) opts.rexpire = parseInt(process.env.REDIS_EXPIRE)
+					
 					if (opts.rpath || opts.rfield) {
 						_args.splice(2, 0, (opts.rpath || opts.rfield))
 					} else if (redis_methods[1] === 'set' && opts.rexpire > 0) {
@@ -205,25 +228,22 @@ class DbConnection {
 					}
 					if (opts.rawait) _args.push(() => resolve(cb_result))
 					let _commands = []
-					opts.rkeepttl ? this.r(..._args) : _commands.push(_args)
+					opts.rkeepttl ? refuncs.r(..._args) : _commands.push(_args)
 
 					if (opts.rexpire > 0 && redis_methods[1] !== 'set') {
 						let _set_expire = true
 						if (opts.rkeepttl) {
-							let _ttl = await this.rr('ttl', opts.rkey)
+							let _ttl = await refuncs.rr('ttl', opts.rkey)
 							if (_ttl > 0) _set_expire = false
 						}
-						if (_set_expire) opts.rkeepttl ? this.r('expire', opts.rkey, opts.rexpire) :  _commands.push(['expire', opts.rkey, opts.rexpire])
+						if (_set_expire) opts.rkeepttl ? refuncs.r('expire', opts.rkey, opts.rexpire) :  _commands.push(['expire', opts.rkey, opts.rexpire])
 					}
-					if (_commands.length) _commands.length > 1 ? this.rpipemulti(_commands) : this.r(..._commands[0])
+					if (_commands.length) _commands.length > 1 ? refuncs.rpipemulti(_commands) : refuncs.r(..._commands[0])
 				}
 			})
 		}
 		return new Promise(f)
-	}
-
-
-
 }
 
-module.exports = new DbConnection()
+
+module.exports = qry
